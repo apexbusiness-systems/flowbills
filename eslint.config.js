67bc24de843bedd14dc65193db03cbae8302200a
Apex Business Apps <s4784f7nrc@privaterelay.appleid.com>
2025-12-06 18:31:41 -0700

    Adjust lint config and assert JSON imports

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,34 @@
-import js from "@eslint/js";
-import globals from "globals";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+const browserGlobals = {
+  document: "readonly",
+  window: "readonly",
+  navigator: "readonly",
+};
+
+export default [
   { ignores: ["dist"] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: "module",
+      parser: tsParser,
+      globals: browserGlobals,
     },
     plugins: {
+      "@typescript-eslint": tsPlugin,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
     },
   },
-);
+];
